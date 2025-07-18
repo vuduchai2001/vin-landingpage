8592af7228c6d5aaff0d4648d5e89883a64e1760
vudaian24 <vudaian12a7@gmail.com>
2025-07-18 17:36:04 +0700

    refactor code

---
+++ src/data/mock-data.ts
@@ -0,0 +1,78 @@
+export const bannerImages = [
+  {
+    src: '/images/slider-home/banner-vinfast-vf8-24726-pc-08-1536x496.jpg',
+    alt: 'VinFast Background',
+  },
+  {
+    src: '/images/slider-home/vinfast-vf9-241212-banner-pc01-2048x661.jpg',
+    alt: 'Banner VF8',
+  },
+]
+
+export const vinfastCars = [
+  {
+    name: 'VINFAST VF 3',
+    price: '299.000.000 VNĐ',
+    image: '/images/cars/vinfast-vf3.jpg',
+  },
+  {
+    name: 'VINFAST VF 5',
+    price: '529.000.000 VNĐ',
+    image: '/images/cars/vinfast-vf5.jpg',
+  },
+  {
+    name: 'VINFAST VF 6',
+    price: '689.000.000 VNĐ',
+    image: '/images/cars/vinfast-vf6.jpg',
+  },
+  {
+    name: 'VINFAST VF 7',
+    price: '799.000.000 VNĐ',
+    image: '/images/cars/vinfast-vf7.jpg',
+  },
+  {
+    name: 'VINFAST VF 8',
+    price: '1.019.000.000 VNĐ',
+    image: '/images/cars/vinfast-vf8.jpg',
+  },
+  {
+    name: 'VINFAST VF 9',
+    price: '1.499.000.000 VNĐ',
+    image: '/images/cars/vinfast-vf9.jpg',
+  },
+]
+
+export const greenCars = [
+  {
+    name: 'VINFAST MINIO GREEN',
+    price: '269.000.000 VNĐ',
+    seats: '4 chỗ',
+    range: '>170 km/lần sạc',
+    power: '20 kw',
+    image: '/images/cars/vinfast-minio.png',
+  },
+  {
+    name: 'VINFAST HERIO GREEN',
+    price: '499.000.000 VNĐ',
+    seats: '5 chỗ',
+    range: '326 km/lần sạc',
+    power: '100 kw',
+    image: '/images/cars/vinfast-herio.png',
+  },
+  {
+    name: 'VINFAST NERIO GREEN',
+    price: '668.000.000 VNĐ',
+    seats: '5 chỗ',
+    range: '318,6 km/lần sạc',
+    power: '110 kw',
+    image: '/images/cars/vinfast-nerio.png',
+  },
+  {
+    name: 'VINFAST LIMO GREEN',
+    price: '749.000.000 VNĐ',
+    seats: '7 chỗ',
+    range: '450 km/lần sạc',
+    power: '150 kw',
+    image: '/images/cars/vinfast-limo.png',
+  },
+]
