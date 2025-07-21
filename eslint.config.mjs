e0267841b04c7dbabd68e7d9afc8a55e605f8fcd
vudaian24 <vudaian12a7@gmail.com>
2025-07-21 14:55:40 +0700

    fix build

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,18 +1,16 @@
-import { FlatCompat } from '@eslint/eslintrc'
- 
+import { dirname } from "path";
+import { fileURLToPath } from "url";
+import { FlatCompat } from "@eslint/eslintrc";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
 const compat = new FlatCompat({
-  baseDirectory: import.meta.dirname,
-})
- 
+  baseDirectory: __dirname,
+});
+
 const eslintConfig = [
-  ...compat.config({
-    extends: ['next'],
-    settings: {
-      next: {
-        rootDir: 'packages/my-app/',
-      },
-    },
-  }),
-]
- 
-export default eslintConfig
+  ...compat.extends("next/core-web-vitals", "next/typescript"),
+];
+
+export default eslintConfig;
